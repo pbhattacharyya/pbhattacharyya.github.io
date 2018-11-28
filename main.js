b5b568153c0d81d88ccc510a94c8dbf290e434be
Po Bhattacharyya <po@Pos-MacBook-Pro.local>
2018-11-28 14:13:03 -0500

    fonts

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 
 $( document ).ready(function() {
     console.log("ready!");
-
-
-    $( ".previous" ).click(function() {
-      changePhoto();
-    });
     
-    $( ".next" ).click(function() {
+    $( document ).click(function() {
       changePhoto();
+      console.log("click!");
     });
+
+
+     // Swipe to remove list item
+     $( document ).on( "swipeleft swiperight", ".card-area", function( event ) {
+      console.log("swipe!");
+      changePhoto();
+
+  });
+  // If it's not a touch device...
+  if ( ! $.mobile.support.touch ) {
+      // Remove the class that is used to hide the delete button on touch devices
+      $( "#list" ).removeClass( "touch" );
+      // Click delete split-button to remove list item
+      $( ".delete" ).on( "click", function() {
+          var listitem = $( this ).parent( "li" );
+          confirmAndDelete( listitem );
+      });
+  }
+
+    // $( ".card-area" ).on( "swipe", function() {
+    //   changePhoto();
+    //   console.log("swipe!");
+
+    // });
 
     function changePhoto() {
 
