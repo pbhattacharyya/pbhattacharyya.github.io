631de15d46d87e72882b4bd39bfdaac72b9d741f
Po Bhattacharyya <po@Pos-MacBook-Pro.local>
2018-11-29 17:43:19 -0500

    mobile

--- main.js
+++ main.js
@@ -195,29 +195,10 @@ var myLineChart = new Chart(ctx, startingData);
       console.log("click!");
     });
 
-
-     // Swipe to remove list item
-     $( document ).on( "swipeleft swiperight", ".card-area", function( event ) {
-      console.log("swipe!");
+    $(document ).on("tap",function(){
       changePhoto();
-
-  });
-  // If it's not a touch device...
-  if ( ! $.mobile.support.touch ) {
-      // Remove the class that is used to hide the delete button on touch devices
-      $( "#list" ).removeClass( "touch" );
-      // Click delete split-button to remove list item
-      $( ".delete" ).on( "click", function() {
-          var listitem = $( this ).parent( "li" );
-          confirmAndDelete( listitem );
-      });
-  }
-
-    // $( ".card-area" ).on( "swipe", function() {
-    //   changePhoto();
-    //   console.log("swipe!");
-
-    // });
+      console.log("click!");
+    });
 
     function changePhoto() {
 
